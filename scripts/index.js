843ec41e481bd52e40143bff179234e94c5b1a98
troy-inspify <hoanghai31299@gmail.com>
2023-01-16 19:09:31 +0700

    add setting option to customize prize

---
+++ scripts/index.js
@@ -0,0 +1,307 @@
+import Setting from "./setting.js";
+const audio = new Audio("./tick.mp3");
+//animation;
+const timeline = gsap.timeline();
+const winTimeline = gsap.timeline({ paused: true });
+const winAnimate = winTimeline
+  .to(
+    ".prize-noti",
+    {
+      scale: 1,
+      opacity: 1,
+      ease: "back",
+      duration: 1,
+    },
+    "start"
+  )
+  .to(".blur-bg", {
+    visibility: "visible",
+    opacity: 1,
+    ease: "back",
+    duration: 0.5,
+  })
+  .to(
+    ".prize-value",
+    {
+      visibility: "visible",
+      duration: 0.5,
+      top: 20,
+    },
+    "end"
+  );
+timeline
+  .to(
+    ".wrapper",
+    {
+      css: {
+        visibility: "visible",
+      },
+    },
+    "start"
+  )
+  .from("h1", {
+    duration: 1.3,
+    y: 40,
+    skewY: 22,
+    opacity: 0,
+  })
+  .from(
+    ".canvas-wrapper",
+    {
+      duration: 1,
+      y: 20,
+      rotate: 120,
+      ease: "back",
+      opacity: 0,
+    },
+    "createCanvas"
+  )
+  .from("button.spin", {
+    duration: 1,
+    y: 20,
+    scale: 1.2,
+    ease: "bounce",
+    opacity: 0,
+  });
+
+const pointer = document.querySelector(".pointer");
+const pointerAnimation = gsap.to(pointer, {
+  scale: 1.3,
+  duration: 0.5,
+  rotate: 360,
+  paused: true,
+});
+
+const playSound = () => {
+  audio.pause();
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const money = {
+  10000: "./images/money/10k.png",
+  5000: "./images/money/5k.jpg",
+  20000: "./images/money/20k.jpg",
+  50000: "./images/money/50k.jpg",
+  30000: "./images/money/30k.png",
+  100000: "./images/money/100k.jpg",
+  200000: "./images/money/200k.jpg",
+  500000: "./images/money/500k.jpg",
+};
+function onSaveSetting() {
+  colourWheel = createWeel();
+  colourWheel.draw();
+  const settingContainer = document.querySelector(".modal.setting");
+  settingContainer.classList.remove("open");
+  timeline.play("createCanvas");
+}
+
+//setting prizes
+const parentElement = document.querySelector(".setting-prizes");
+const settings = new Setting(parentElement, onSaveSetting);
+settings.renderSettingUI();
+
+const getRandomSpin = () => Math.floor(8 + Math.random() * 10);
+
+const getRandomDuration = () => Math.floor(8 + Math.random() * 5);
+
+function winAnimation() {
+  const winningSegmentNumber = colourWheel.getIndicatedSegmentNumber();
+
+  const winningPrize = colourWheel.getIndicatedSegment();
+  for (let x = 1; x < colourWheel.segments.length; x++) {
+    colourWheel.segments[x].fillStyle = "gray";
+  }
+
+  colourWheel.segments[winningSegmentNumber].fillStyle = "yellow";
+  const moneyKey = winningPrize.text.replace(/\D/g, "");
+  console.log(moneyKey);
+  colourWheel.draw();
+  addToHistory(moneyKey);
+  document.querySelector(".prize-value").src = money[moneyKey];
+  document.querySelector(".prize").innerHTML = `${winningPrize?.text}`;
+  winAnimate.play("start");
+  window.addEventListener("click", (e) => {
+    if (e.target !== document.querySelector(".prize-noti")) {
+      winAnimate.reverse();
+      pointerAnimation.reverse();
+      resetWheel();
+    }
+  });
+}
+
+const createWeel = () => {
+  console.log("create weel");
+  const settingPrize = settings.getSettings();
+  const prizes = settingPrize.map((prize) => {
+    return {
+      text: settings.formatValueToText(prize.value * 1000),
+      fillStyle: prize.color,
+      size: winwheelPercentToDegrees(prize.rate),
+    };
+  });
+  console.log(prizes);
+  return new Winwheel({
+    canvasId: "luckydraw",
+    numSegments: prizes.length,
+    lineWidth: 0.5,
+    responsive: true,
+    pointerAngle: 90,
+    segments: prizes,
+    animation: {
+      easing: "Power4.out",
+      type: "spinToStop", // Type of animation.
+      duration: getRandomDuration(), // How long the animation is to take in seconds.
+      spins: getRandomSpin(), // The number of complete 360 degree rotations the wheel is to do.
+      callbackFinished: winAnimation,
+      callbackSound: playSound, // Specify function to call when sound is to be triggered.
+      soundTrigger: "pin",
+    },
+    pins: {
+      number: 8,
+      outerRadius: 3,
+      fillStyle: "#FFCF99",
+      strokeStyle: "#FFFFFF",
+    },
+  });
+};
+
+let colourWheel = createWeel();
+
+const resetWheel = () => {
+  const settingPrize = settings.getSettings();
+  const prizes = settingPrize.map((prize) => {
+    return {
+      text: settings.formatValueToText(prize.value * 1000),
+      fillStyle: prize.color,
+      size: winwheelPercentToDegrees(prize.rate),
+    };
+  });
+  for (let x = 1; x < colourWheel.segments.length; x++) {
+    colourWheel.segments[x].fillStyle = prizes[x - 1].fillStyle;
+  }
+  colourWheel.rotationAngle = 0;
+  colourWheel.animation.duration = getRandomDuration();
+  colourWheel.animation.spins = getRandomSpin();
+
+  colourWheel.draw();
+};
+
+const btn = document.querySelector(".button.spin");
+btn.addEventListener("click", () => {
+  pointerAnimation.play();
+  colourWheel.startAnimation();
+});
+
+function addToHistory(prize) {
+  const history = localStorage.getItem("spinHistory") || "[]";
+  const date = moment().format("DD/MM, HH:mm");
+  const historyList = JSON.parse(history);
+  historyList.push({
+    date,
+    prize,
+  });
+  localStorage.setItem("spinHistory", JSON.stringify(historyList));
+}
+
+function getHistory() {
+  const history = localStorage.getItem("spinHistory") || "[]";
+  const historyList = JSON.parse(history);
+  return historyList;
+}
+
+function renderHistory() {
+  const historyList = getHistory();
+  const historyContainer = document.querySelector(".list table tbody");
+  let total = 0;
+  historyContainer.innerHTML = "";
+  historyList.forEach((prize) => {
+    const row = document.createElement("tr");
+    const sttColumn = document.createElement("th");
+    const dateColumn = document.createElement("td");
+    const prizeColumn = document.createElement("td");
+    sttColumn.innerHTML = historyList.indexOf(prize) + 1;
+    dateColumn.innerHTML = prize.date;
+    prizeColumn.innerHTML = settings.formatValueToText(+prize.prize);
+    row.appendChild(sttColumn);
+    row.appendChild(dateColumn);
+    row.appendChild(prizeColumn);
+    historyContainer.appendChild(row);
+    total += parseInt(prize.prize);
+  });
+  document.querySelector(".total span").innerHTML =
+    settings.formatValueToText(total);
+}
+let isOpen = false;
+const btnExpandControl = document.querySelector(".button.expand-control");
+btnExpandControl.addEventListener("click", () => {
+  if (isOpen) {
+    expandAnimate.reverse();
+    isOpen = false;
+    return;
+  }
+  expandAnimate.play("start");
+  isOpen = true;
+});
+
+const btnHistory = document.querySelector(".button.open-history");
+btnHistory.addEventListener("click", () => {
+  renderHistory();
+  const historyContainer = document.querySelector(".modal.history");
+  historyContainer.classList.add("open");
+  btnExpandControl.dispatchEvent(new Event("click"));
+});
+
+const btnCloseHistory = document.querySelector(".button.close-history");
+btnCloseHistory.addEventListener("click", () => {
+  const historyContainer = document.querySelector(".modal.history");
+  historyContainer.classList.remove("open");
+});
+
+const btnDeleteHistory = document.querySelector(".button.clear-history");
+btnDeleteHistory.addEventListener("click", () => {
+  localStorage.removeItem("spinHistory");
+  renderHistory();
+});
+
+const btnOpenSetting = document.querySelector(".button.open-setting");
+btnOpenSetting.addEventListener("click", () => {
+  const settingContainer = document.querySelector(".modal.setting");
+  settingContainer.classList.add("open");
+  btnExpandControl.dispatchEvent(new Event("click"));
+});
+
+const btnCloseSetting = document.querySelector(".button.close-setting");
+btnCloseSetting.addEventListener("click", () => {
+  const settingContainer = document.querySelector(".modal.setting");
+  settingContainer.classList.remove("open");
+});
+
+const expandTl = gsap.timeline({ paused: true });
+const expandAnimate = expandTl.fromTo(
+  ".control-list .button",
+  {
+    top: -30,
+  },
+  {
+    top: 0,
+    duration: 0.5,
+    opacity: 1,
+    visibility: "visible",
+    ease: "back",
+    stagger: 0.1,
+  },
+  "start"
+);
+
+const fullScreen = document.querySelector(".button.full-screen");
+fullScreen.addEventListener("click", () => {
+  if (document.fullscreenElement) {
+    document.exitFullscreen();
+    fullScreen.classList.remove("active");
+  } else {
+    document.documentElement.requestFullscreen();
+    fullScreen.classList.add("active");
+  }
+  btnExpandControl.dispatchEvent(new Event("click"));
+});
---
+++ scripts/setting.js
@@ -0,0 +1,204 @@
+"use strict";
+
+const valueArray = [5, 30, 10, 20, 50, 100, 200, 500];
+const defaultPrizes = [
+  {
+    color: "#8DCBE6",
+    value: 10,
+    rate: 40,
+  },
+
+  {
+    color: "#FFEA20",
+    value: 5,
+    rate: 20,
+  },
+
+  {
+    color: "#9DF1DF",
+    value: 20,
+    rate: 20,
+  },
+
+  {
+    color: "#E3F6FF",
+    value: 50,
+    rate: 5,
+  },
+  {
+    color: "#C97064",
+    value: 30,
+    rate: 15,
+  },
+];
+const defaultSetting = defaultPrizes;
+class Setting {
+  constructor(parentElement, onSave) {
+    this._settings = {};
+    this.loadSetting();
+    this._onSave = onSave;
+    this.parentElement = parentElement;
+  }
+
+  loadSetting() {
+    try {
+      this._settings =
+        JSON.parse(localStorage.getItem("spin_settings")) || defaultSetting;
+    } catch (error) {
+      this._settings = defaultSetting;
+    }
+  }
+
+  saveToStorage() {
+    localStorage.setItem("spin_settings", JSON.stringify(this._settings));
+  }
+
+  createColorInput = (defaultValue) => {
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("control");
+    const input = document.createElement("input");
+    input.style.width = "50px";
+    input.classList.add("input");
+    input.type = "color";
+    input.value = defaultValue;
+    wrapper.append(input);
+    return wrapper;
+  };
+
+  createSelect = (defaultValue) => {
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("control", "is-expanded");
+    const span = document.createElement("span");
+    span.classList.add("select", "is-fullwidth");
+    const select = document.createElement("select");
+    const options = valueArray.map((value) => {
+      const option = document.createElement("option");
+      option.value = value;
+      option.innerText = this.formatValueToText(value * 1000);
+      return option;
+    });
+    select.append(...options);
+    select.value = defaultValue;
+    span.append(select);
+    wrapper.append(span);
+    return wrapper;
+  };
+
+  createInput(defaultValue) {
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("control");
+    const input = document.createElement("input");
+    input.classList.add("input");
+    input.type = "number";
+    input.min = 1;
+    input.max = 100;
+    input.step = 5;
+    input.value = defaultValue;
+    wrapper.append(input);
+    return wrapper;
+  }
+
+  formatPrize(value) {
+    //input: string format 1.000.000 usd
+    //output: number value
+    return value.replace(/\D/g, "");
+  }
+
+  formatValueToText(value) {
+    //input: number value
+    //output: string format 1.000.000 usd
+    return value.toLocaleString("vi-VN", {
+      style: "currency",
+      currency: "VND",
+    });
+  }
+
+  validateSetting(settings) {
+    const totalRate = settings.reduce((acc, cur) => acc + +cur.rate, 0);
+    console.log(totalRate);
+    if (totalRate !== 100) {
+      return false;
+    }
+    return true;
+  }
+
+  resetSetting() {
+    this._settings = defaultSetting;
+    this.renderSettingUI();
+    this.saveToStorage();
+    alert("Luu thanh cong");
+    this._onSave();
+  }
+
+  createSaveButtons() {
+    const wrapper = document.createElement("div");
+    wrapper.classList.add("field", "mt-5", "has-text-right");
+    const saveButton = document.createElement("button");
+    saveButton.classList.add("button", "save-setting");
+    saveButton.innerText = "Lưu";
+    saveButton.addEventListener("click", () => this.saveSetting());
+    const resetButton = document.createElement("button");
+    resetButton.classList.add("button", "reset-setting", "is-danger", "mr-2");
+    resetButton.innerText = "Reset";
+    resetButton.addEventListener("click", () => this.resetSetting());
+    wrapper.append(resetButton, saveButton);
+    return wrapper;
+  }
+
+  renderSettingUI() {
+    const parentElement = this.parentElement;
+    parentElement.innerHTML = "";
+    const fields = [];
+    const prizes = this._settings;
+    for (let i = 0; i < prizes.length; i++) {
+      const field = document.createElement("div");
+      field.classList.add("field", "has-addons");
+      const lable = document.createElement("label");
+      lable.classList.add("label");
+      lable.innerText = `${i + 1}`;
+      const select = this.createSelect(prizes[i].value);
+      field.append(select);
+      const input = this.createInput(prizes[i].rate);
+      field.append(input);
+      const colorInput = this.createColorInput(prizes[i].color);
+      field.append(colorInput);
+      fields.push(lable, field);
+    }
+    parentElement.append(...fields);
+    const saveButton = this.createSaveButtons();
+    parentElement.append(saveButton);
+  }
+
+  saveSetting() {
+    const fields = document.querySelectorAll(".field.has-addons");
+    const newSettings = [];
+    for (let i = 0; i < fields.length; i++) {
+      const field = fields[i];
+      const value = +field.querySelector(".select select").value;
+      const rate = +field.querySelector(".input").value;
+      const color = field.querySelector(".input[type=color]").value;
+      newSettings.push({
+        color,
+        value,
+        rate,
+      });
+    }
+
+    const isValidRate = this.validateSetting(newSettings);
+    if (!isValidRate) {
+      alert("Tỉ lệ phần thưởng không hợp lệ");
+      return;
+    } else {
+      alert("Lưu thành công");
+      this._settings = newSettings;
+      this.saveToStorage();
+      this._onSave();
+    }
+  }
+
+  getSettings() {
+    return this._settings.filter((setting) => setting.rate > 0);
+  }
+}
+
+export default Setting;
